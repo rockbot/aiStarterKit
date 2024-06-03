199226eb9ed28839af1b4adb6203b0ed7e228218
rockbot <raquel@rckbt.me>
2024-06-02 17:47:24 -0700

    SUPER basic sentiment analysis using `sentiment` npm module

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    Sentiment = require('sentiment');
+
+var router = express.Router(),
+    sentiment = new Sentiment();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sentiment Analyzer' });
+});
+
+router.get('/get-data', function(req,res,next) {
+  console.log(req.query);
+
+  var result = sentiment.analyze(req.query.phrase);
+  console.log(result);
+
+  var sentimentResult = result.score > 0 ? "positive" : "negative";
+  res.render('index', { title: 'Sentiment Analyzer ', sentiment: sentimentResult }) 
 });
 
 module.exports = router;
